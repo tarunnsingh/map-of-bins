67abf93cdaac968c56beb95c29b4ce8c336d09b9
Tarun Singh <tarunnsingh2019@gmail.com>
2020-10-24 23:26:26 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-const MONGODB_URI = "mongodb+srv://addverb_user:addverb_password@addverb-clean-city.g3gqa.mongodb.net/clean_city?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = { MONGODB_URI, JWT_SECRET_KEY };
